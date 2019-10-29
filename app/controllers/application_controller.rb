08a219ee75270c5839db8fc0b3b804fda380792f
Ned-el-ch <nedelchev.niki@gmail.com>
2019-10-29 13:02:40 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,26 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+
+        erb :index
+        
+    end
+
+    post '/teams' do
+
+        @team = Team.new(params[:team])
+
+        params[:team][:heroes].each do |deets|
+
+            Hero.new(deets)
+            
+        end
+
+        @heroes = Hero.all
+
+        erb :team
+        
+    end
 
 end
